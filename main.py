2e7d2dde5640b50dc1d9168c2e189762be6d89d0
ilesanmi praise <gbengapraise3@gmail.com>
2024-10-27 11:09:28 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,33 @@
+import random
+
+def game():
+    while True:
+        user_choice = input("Enter a choice (rock, paper, scissors): ").lower()
+        possible_choices = ["rock", "paper", "scissors"]
+        computer_choice = random.choice(possible_choices)
+        print(f"\nUser chose {user_choice}, computer chose {computer_choice}.\n")
+
+        if user_choice == computer_choice:
+            print(f"Both players selected {user_choice}. It's a tie!")
+        elif user_choice == "rock":
+            if computer_choice == "scissors":
+                print("Rock smashes scissors! User wins!")
+            else:
+                print("Paper covers rock! Computer wins.")
+        elif user_choice == "paper":
+            if computer_choice == "rock":
+                print("Paper covers rock! User wins!")
+            else:
+                print("Scissors cuts paper! Computer wins.")
+        elif user_choice == "scissors":
+            if computer_choice == "paper":
+                print("Scissors cuts paper! User wins!")
+            else:
+                print("Rock smashes scissors! Computer wins.")
+
+        play_again = input("Play again? (yes/no): ").lower()
+        if play_again != "yes":
+            break
+
+if __name__ == "__main__":
+    game()
